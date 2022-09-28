9320397eea27a18729e7106533387613e12dc003
seanomeara96 <seanom96@gmail.com>
2022-09-28 09:34:20 +0100

    attempt at caching issue

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
     function dcphysio_files(){
         wp_enqueue_script("main_script", get_theme_file_uri("/build/main.js"), NULL, "1.0", true);
         wp_enqueue_style("main_styles", get_theme_file_uri("/build/main.css"));
+       // ? use something like this? //wp_enqueue_style("main_styles", get_theme_file_uri("/build/main.css"), array(), filemtime(get_theme_file_uri("/build/main.css")), false);
     }
 
     add_action("wp_enqueue_scripts", "dcphysio_files");
